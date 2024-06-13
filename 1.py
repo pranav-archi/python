f60e7ed39adca49dae8a08c78707ad6603c4e345
pranav-archi <158431652+pranav-archi@users.noreply.github.com>
2024-06-13 20:08:50 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,12 +1,17 @@
-f1=open('convert.txt','r')
-for i in range(0,3,1):   
-    info1=f1.readline().replace('\n','')
-    print(info1)
-    list1=info1.split(' ')
-    lhs_value=float(list1[0])
-    lhs_units=list1[1]
-    rhs_value=float(list1[3])
-    rhs_units=list1[4]
-    print(1,lhs_units,'=',(rhs_value/lhs_value),rhs_units)
-    print(1,rhs_units,'=',(lhs_value/rhs_value),lhs_units)
-    print()
+def convert1(fname):    
+    f1=open(fname,'r')
+    list1=fname.split('.')
+    f2=open(list1[0]+'_out.'+list1[1],'w')
+    for i in range(0,3,1):   
+        info1=f1.readline().replace('\n','')
+        print(info1)
+        list1=info1.split(' ')
+        lhs_value=float(list1[0])
+        lhs_units=list1[1]
+        rhs_value=float(list1[3])
+        rhs_units=list1[4]
+        line1=str(str(1)+' '+lhs_units+' = '+str((rhs_value/lhs_value))+' '+rhs_units)
+        line2=str(str(1)+' '+rhs_units+' = '+str((lhs_value/rhs_value))+' '+lhs_units)       
+        f2.write(line1+'\n'+line2+'\n'+'\n')
+    f2.close()
+convert1('convert.txt')
